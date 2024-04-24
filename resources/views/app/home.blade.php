11cd813e05c8f20f8898f6f388e6b04f068ce9bb
warcat745 <reskydianto71@gmail.com>
2024-04-24 12:43:38 +0700

    e-ptsp ver 1.0

---
+++ resources/views/app/home.blade.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E-PTSP IAIN Syaikh Abdurrahman Siddik Bangka Belitung</title>
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <!-- Navbar Section -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow-sm">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">E-PTSP</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-current="page" href="#"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/slider.svg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="images/gambar1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="images/gambar3.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <img src="images/Group1.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">E-Campus</h5>
+                        <p class="card-text">Platform digital untuk manajemen dan akses kegiatan akademik dan
+                            administrasi kampus, memudahkan interaksi antara mahasiswa, dosen, dan staf.</p>
+                        <a href="https://ecampus.iainsasbabel.ac.id/" class="btn btn-primary" target="_blank">Kunjungi
+                            Halaman</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <img src="images/Group2.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">E-SPMI</h5>
+                        <p class="card-text">Sistem evaluasi dan monitoring untuk memastikan kualitas pendidikan di
+                            lembaga pendidikan tinggi, mendukung peningkatan terus-menerus.</p>
+                        <a href="https://e-spmiiainsas.com/" class="btn btn-primary" target="_blank">Kunjungi
+                            Halaman</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <img src="images/Group3.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">E-Planning</h5>
+                        <p class="card-text">Proses perencanaan digital yang menggunakan teknologi untuk mengatur,
+                            mengelola, dan memonitor proyek dan kegiatan secara efisien.</p>
+                        <a href="https://e-planingsystem.com/" class="btn btn-primary" target="_blank">Kunjungi
+                            Halaman</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4 mb-4">
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <img src="images/Group4.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">E-Sigma</h5>
+                        <p class="card-text">Sistem komprehensif untuk pengelolaan data dan operasi administratif,
+                            meningkatkan efisiensi dan mendukung pengambilan keputusan.</p>
+                        <a href="https://e-sigma-iainsasbabel.com/" class="btn btn-primary" target="_blank">Kunjungi
+                            Halaman</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <img src="images/Group5.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">E-BKD</h5>
+                        <p class="card-text">Dokumentasi aktivitas akademik dosen untuk evaluasi kinerja, mendukung
+                            proses audit, promosi, dan insentif.</p>
+                        <a href="#" class="btn btn-primary">Kunjungi Halaman</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <img src="images/Group6.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">E-MicroTeaching</h5>
+                        <p class="card-text">Praktik mengajar online dalam sesi singkat untuk mengembangkan
+                            keterampilan mengajar dengan umpan balik instan dalam lingkungan yang terkontrol.</p>
+                        <a href="#" class="btn btn-primary">Kunjungi Halaman</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer Section -->
+    <footer class="footer">
+        <div class="container">
+            <span class="text-muted">Jl. Raya Petaling Km.13, Petaling, Mendo Barat, Kabupaten Bangka, Kepulauan Bangka
+                Belitung 33173</span><br>
+            <span class="text-muted">(0717) 9108206</span>
+        </div>
+    </footer>
+
+</body>
+
+</html>
